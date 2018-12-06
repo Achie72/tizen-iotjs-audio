f2239c18ac4ab0de2e0389df6d8f64048f3a39b9
Istvan Miklos <imiklos2@inf.u-szeged.hu>
2018-12-06 10:47:22 +0100

    init

---
+++ inc/tizen-iotjs-audio.h
@@ -0,0 +1,12 @@
+#ifndef __tizen-iotjs-audio_H__
+#define __tizen-iotjs-audio_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "tizen-iotjs-audio"
+
+
+#endif /* __tizen-iotjs-audio_H__ */
---
+++ res/index.js
@@ -0,0 +1,34 @@
+var https = require('https')
+var fs = require('fs');
+
+console.log(fs.existsSync('/home/owner/apps_rw/org.example.almafa/res/test.mp3'));
+
+var fd = fs.openSync('/home/owner/apps_rw/org.example.almafa/res/test.mp3', 'r');
+
+var stat = fs.fstatSync(fd);
+
+console.log(stat);
+
+var buffer = new Buffer(stat.size);
+var bytesRead = fs.readSync(fd, buffer, 0, buffer.length, 0);
+console.log(bytesRead);
+
+var options = {
+  method: 'POST',
+  host: 'api.wit.ai',
+  headers: {'Authorization': 'Bearer VZE6UQD4FZJQZZ65CJCSNMC23EGEJ7N7',
+            'Content-Type': 'audio/mpeg3',
+            'Content-length': bytesRead},
+  path: '/speech?',
+  rejectUnauthorized: false,
+};
+
+var request = https.request(options, function(response) {
+
+  response.on('data', function(chunk) {
+		console.log(chunk.toString());
+  });
+
+});
+request.write(buffer);
+request.end();
